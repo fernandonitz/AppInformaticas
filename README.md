e738da32128ae19c5121589fd3809ae527fa1f8f
fernandonitz <nitzfernando@gmail.com>
2017-06-02 23:50:38 -0300

    VER README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AppInformaticas
+
+Va en resumen lo que dejo de info por aca...
+En asd: va un juego, dezipea y ejecuta el html giochi 20, que esta muy copado, es el tetris.
+En colectivos: es una muestra cagada de una idea, nada importante.
+tetris: nada importante, del giochi 20 probe un par de giladas.
+
+
+IMPORTANTE: 
+webs: diseños muy buenos de webs y como organizan las webs.
+prueba: donde estoy laburando, lo ultimo es un pixel que se va moviendo de forma horizontal por la pantalla.
--- prueba/codigo.js
+++ prueba/codigo.js
@@ -1,16 +1,10 @@
 var a = screen.height
 var b = screen.width
-var div = document.getElementById("cole1");
-div.style.left = '10px'
-
-function prueba(){
-//for (var i = 0; i >= 50; i++) {
-	document.getElementById("cole1").style.left = document.getElementById("cole1").style.left + '20px';
-}
-
-var timerINT = 500 
-function interval1(){
-  clearTimeout(timerID)
-  prueba()
-  timerID = setTimeout("interval1()",timerINT)
+document.getElementById("cole1").style.left= String(100) + "px";
+prueba()
+a = 100
+function prueba(){	
+		a = a + 10	
+		document.getElementById("cole1").style.left= String(a) + "px";
+		setTimeout(prueba,2000)
 }
